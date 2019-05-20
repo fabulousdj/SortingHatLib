3ff57b3acd918ed5282630b9a98994541b4562df
pvn25 <pvn251@gmail.com>
2019-05-19 17:14:52 -0700

    testing

--- setup.py
+++ setup.py
@@ -2,19 +2,20 @@ from setuptools import setup
 
 setup(
     # Needed to silence warnings
-    name='ML Feature Type Inference',
-    url='https://github.com/pvn25/SortingHatLib',
-    author='Vraj Shah',
-    author_email='vps002@eng.ucsd.edu',
+    name='Measurements',
+    url='https://github.com/jladan/package_demo',
+    author='John Ladan',
+    author_email='jladan@uwaterloo.ca',
     # Needed to actually package something
-    packages=['sortinghat'],
+    packages=['measure'],
     # Needed for dependencies
     install_requires=['numpy'],
     # *strongly* suggested for sharing
     version='0.5',
     license='MIT',
+    description='An example of a python package from pre-existing code',
     # We will also need a readme eventually (there will be a warning)
-#     long_description=open('README.rst').read(),
+    long_description=open('README.rst').read(),
     # if there are any scripts
-    scripts=['scripts/test.py'],
+    scripts=['scripts/hello.py'],
 )
