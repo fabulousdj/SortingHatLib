2552d0f401b7dd00f7d7a14cbab01bfbb76ce2bc
Vraj <pvn251@gmail.com>
2019-05-28 20:51:03 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,14 @@
 from setuptools import setup
 
 setup(
-    # Needed to silence warnings
     name='SortingHat Library',
     url='https://github.com/pvn25/SortingHatLib',
     author='Vraj Shah',
     author_email='pvn251@gmail.com',
-    # Needed to actually package something
     packages=['sortinghat'],
-    # Needed for dependencies
     install_requires=['numpy'],
-    # *strongly* suggested for sharing
     version='0.5',
     license='MIT',
     description='An example of a python package from pre-existing code',
-    # We will also need a readme eventually (there will be a warning)
-#     long_description=open('README.rst').read(),
-    # if there are any scripts
     scripts=['scripts/hello.py'],
 )
